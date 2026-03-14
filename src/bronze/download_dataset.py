6f750e3a2a119d71a651a4bce2c9ee8115177eec
Muhammad Rezky Hamesi Akhyar <muhammadrezkyhamesi@gmail.com>
2026-03-14 14:25:51 +0700

    [feature] fix dbutils

--- src/bronze/download_dataset.py
+++ src/bronze/download_dataset.py
@@ -7,20 +7,17 @@ from src.config.constants import (
 )
 
 
-def download_dataset():
+def download_dataset(dbutils):
 
     local_tmp = "/tmp/yellow_tripdata_2023-01.parquet"
 
     print("Downloading dataset...")
-
     urllib.request.urlretrieve(DATA_URL, local_tmp)
 
-    print("Creating DBFS directory if not exists...")
-
+    print("Creating DBFS directory...")
     dbutils.fs.mkdirs(DBFS_DATA_DIR)
 
-    print("Moving dataset to DBFS...")
-
+    print("Copying dataset to DBFS...")
     dbutils.fs.cp(f"file:{local_tmp}", DBFS_DATA_PATH)
 
     print("Dataset ready at:", DBFS_DATA_PATH)
